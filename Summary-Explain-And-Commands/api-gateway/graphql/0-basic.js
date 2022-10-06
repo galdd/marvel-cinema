ae748751efdf9cc6535549da5066c27167487784
Gal Dagan <gald2345@gmail.com>
2022-10-06 20:29:08 +0300

    Clean unnecessary files

--- Summary-Explain-And-Commands/api-gateway/graphql/0-basic.js
+++
@@ -1,4 +0,0 @@
-// inspiration
-// https://github.com/safak/youtube/tree/mern-netflix-app
-// https://github.com/lucaschen/microservices-demo
-// https://github.com/lucaschen/microservices-chat-demo
--- Summary-Explain-And-Commands/client/0-Basic.js
+++
@@ -1,5 +0,0 @@
-// inspiration
-// https://github.com/stephen-walcher/reservation-system
-// https://github.com/zitzy/simple-hotels-app
-// https://github.com/NitnekB/movies-graphql
-// https://github.com/safak/youtube/tree/mern-netflix-app
